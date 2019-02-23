61d1a107634ead49f1bb530378fc205de6066d5a
itsmeow <mods.itsmeow@gmail.com>
2019-02-22 22:28:11 -0500

    Fix caps

--- src/main/java/com/builtbroken/deadmanssatchel/SatchelCapabilityProvider.java
+++ src/main/java/com/builtbroken/deadmanssatchel/SatchelCapabilityProvider.java
@@ -5,13 +5,14 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
-public class SatchelCapabilityProvider implements ICapabilitySerializable {
+public class SatchelCapabilityProvider implements ICapabilityProvider, INBTSerializable<NBTTagCompound> {
 
 	private ItemStack stack;
+	private Object handler = null;
 	
 	public SatchelCapabilityProvider(ItemStack stack) {
 		this.stack = stack;
@@ -24,18 +25,26 @@ public class SatchelCapabilityProvider implements ICapabilitySerializable {
 
     @Override
     public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-		if(!stack.hasTagCompound()) {
-			stack.setTagCompound(new NBTTagCompound());
-		}
-		NBTTagCompound compound = stack.getTagCompound();
-		if (compound.hasKey("items")) {
-            ((ItemStackHandler) stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)).deserializeNBT((NBTTagCompound) compound.getTag("items"));
-        }
         if (hasCapability(capability, facing)) {
-            return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(new ItemStackHandler(6));
+        	if(handler == null) {
+        		handler = CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(new ItemStackHandler(6));
+        	}
+            return (T) handler;
         }
 
         return null;
     }
+
+	@Override
+	public NBTTagCompound serializeNBT() {
+		return ((ItemStackHandler) handler).serializeNBT();
+	}
+
+	@Override
+	public void deserializeNBT(NBTTagCompound nbt) {
+		if (nbt.hasKey("items")) {
+            ((ItemStackHandler) handler).deserializeNBT((NBTTagCompound) nbt.getTag("items"));
+        }
+	}
 
 }
